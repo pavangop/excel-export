2bcb9e794f555b197f1e19d7206d45132c1477a3
Pavan Gop <pavangop@ymail.com>
2021-02-07 22:17:44 +0530

    commit 4

--- src/UI/navbar/Navbar.js
+++ src/UI/navbar/Navbar.js
@@ -5,7 +5,7 @@ import '../../../node_modules/bootstrap/dist/js/bootstrap.bundle.min';
 const navbar = (props) => {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-danger ">
-            <div class="text-white font-weight-bold pt-2 pb-2 mx-auto">
+            <div className="text-white font-weight-bold pt-2 pb-2 mx-auto">
                 WLM Requirements
             </div>
         </nav>
